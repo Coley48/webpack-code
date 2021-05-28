aba51d9413093f53307a3cd45322cb5714b560fb
kongyu <kongyu@thinkerx.com>
2021-05-28 08:41:58 +0800

    输出管理

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// https://github.com/jantimon/html-webpack-plugin
 
 // 如果 webpack.config.js 存在，则 webpack 命令将默认选择使用它。
 // console.log(__dirname, __filename);
 // D:\webpack\test D:\webpack\test\webpack.config.js
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
     },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        // 清除 dist 目录
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Hello',
+            template: './src/index.html',
+            // favicon: './src/favicon.ico'
+        }),
+    ],
 };
