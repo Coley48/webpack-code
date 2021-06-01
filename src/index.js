0e9eba19e355a26663f60f652f461afa40bb83a9
kongyu <kongyu@thinkerx.com>
2021-06-01 11:26:26 +0800

    缓存

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import _ from 'lodash'
-import printMe from './print.js';
+// import Print from './print';
+// import $ from 'jquery'
 
 function component() {
     const element = document.createElement('div');
-    const btn = document.createElement('button');
 
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
+    // element.onclick = Print.bind(null, "Hello, world.");
 
     return element;
 }
 
 document.body.appendChild(component());
+
+// $("body").append("<div>Hey, I'm Coley48.</div>")
--- webpack.config.js
+++ webpack.config.js
@@ -9,19 +9,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
         index: './src/index.js',
-        print: './src/print.js',
     },
     output: {
-        filename: '[name].bundle.js',
+        // 可替换模板字符串，占位符
+        // [contenthash] substitution 将根据资源内容创建出唯一 hash。当资源内容发生变化时，[contenthash] 也会发生变化。
+        filename: '[name].[contenthash:8].js',
         path: path.resolve(__dirname, 'dist'),
         // 清除 dist 目录
         clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Hello',
+            title: 'Caching',
             template: './src/index.html',
             // favicon: './src/favicon.ico'
         }),
     ],
+    optimization: {
+        // 固定模块id
+        moduleIds: 'deterministic',
+        // 将 runtime 代码拆分为一个单独的 runtime chunk
+        runtimeChunk: "single",
+        // 将第三方库提取到 vendors chunk
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    }
 };
