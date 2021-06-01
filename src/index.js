19930582b421006b9e1562efbdbd43fd5fa27efb
kongyu <kongyu@thinkerx.com>
2021-06-01 17:56:24 +0800

    创建library

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import numRef from './ref.json';
 
-function component() {
-    const element = document.createElement('div');
-
-    // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
+export function numToWord(num) {
+    return _.reduce(
+        numRef,
+        (accum, ref) => {
+            return ref.num === num ? ref.word : accum;
+        },
+        ''
+    );
 }
 
-document.body.appendChild(component());
+export function wordToNum(word) {
+    return _.reduce(
+        numRef,
+        (accum, ref) => {
+            return ref.word === word && word.toLowerCase() ? ref.num : accum;
+        },
+        -1
+    );
+}
+
+console.log('index.js')
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
-// 如果 webpack.config.js 存在，则 webpack 命令将默认选择使用它。
-// console.log(__dirname, __filename);
-// D:\webpack\test D:\webpack\test\webpack.config.js
-
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: 'webpack-numbers.js',
+        clean: true,
+        // 暴露从入口导出的内容
+        library: "webpackNumbers"
     },
 };
