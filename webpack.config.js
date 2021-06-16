6e3bf22cd9f96485cb0d986dbbdc20c6635d7e00
kongyu <kongyu@thinkerx.com>
2021-06-16 20:08:26 +0800

    环境变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
 
-// 如果 webpack.config.js 存在，则 webpack 命令将默认选择使用它。
-// console.log(__dirname, __filename);
-// D:\webpack\test D:\webpack\test\webpack.config.js
+// 要使用 env 变量，你必须将 module.exports 转换成一个函数：
+module.exports = (env) => {
+    // Use env.<YOUR VARIABLE> here:
+    console.log('Goal: ', env.goal); // 'local'
+    console.log('Output: ', env.output); // dist
+    console.log('Production: ', env.production); // true
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: `${env.goal}.bundle.js`,
+            path: path.resolve(__dirname, env.output),
+            clean: true,
+        },
+    };
 };
