f8b694d076c33b5f4937172bd4b44e606eaf065d
kongyu <kongyu@thinkerx.com>
2021-07-02 08:41:34 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-import _ from 'lodash'
+import _ from "lodash"
+import "./style.css"
+import background from './bg.jpg';
+import Note from './note.xml';
+import Data from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
 
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // 添加图片
+    console.log(background); // http://127.0.0.1:5500/test/dist/8a90cc79488f27cee02f.jpg
+    var myImage = new Image();
+    myImage.src = background;
+    element.appendChild(myImage);
+
+    console.log(Data);
+    console.log(Note);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 // 如果 webpack.config.js 存在，则 webpack 命令将默认选择使用它。
 // console.log(__dirname, __filename);
@@ -7,7 +10,63 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        // 模块 loader 可以链式调用。链中的每个 loader 都将对资源进行转换。链会逆序执行。
+        // 第一个 loader 将其结果（被转换后的资源）传递给下一个 loader，依此类推。
+        // 最后，webpack 期望链中的最后的 loader 返回 JavaScript。
+        rules: [{
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            // webpack 4 需要使用file-loader
+            // webpack 5 内置了加载图片的模块
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            }, {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.toml$/i,
+                type: 'json',
+                parser: {
+                    parse: toml.parse,
+                },
+            }, {
+                test: /\.yaml$/i,
+                type: 'json',
+                parser: {
+                    parse: yaml.parse,
+                },
+            }, {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                },
+            },
+        ],
+
+    },
 };
+
+// Webpack 5
+// asset/resource 发送一个单独的文件并导出 URL。之前通过使用 file-loader 实现。
+// asset/inline 导出一个资源的 data URI。之前通过使用 url-loader 实现。
+// asset/source 导出资源的源代码。之前通过使用 raw-loader 实现。
+// asset 在导出一个 data URI 和发送一个单独的文件之间自动选择。之前通过使用 url-loader，并且配置资源体积限制实现。
